e0669fa5050a08bb157ed4fc05e0ebf16bb99430
王旭东 <wwwxdhouwxd@qq.com>
2021-11-30 20:14:26 +0800

    初步签入

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,63 @@
+import _ from 'lodash';
 import { errorMapper } from './modules/errorMapper'
-import { sayHello } from './modules/utils'
+var roleHarvester = require('harvester');
+var roleUpgrader = require('upgrader');
+var roleBuilder = require('builder');
 
 export const loop = errorMapper(() => {
-    sayHello()
+
+    // 销毁死亡creep
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    // 检查creep数量并补充
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    if(harvesters.length < 2) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName, 
+            {memory: {role: 'harvester'}});
+    }
+    if(Game.spawns['Spawn1'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+        Game.spawns['Spawn1'].room.visual.text(
+            '🛠️' + spawningCreep.memory.role,
+            Game.spawns['Spawn1'].pos.x + 1, 
+            Game.spawns['Spawn1'].pos.y, 
+            {align: 'left', opacity: 0.8});
+    }
+
+    // tower逻辑
+    var tower = Game.rooms[0].find(FIND_MY_STRUCTURES).find(x=>x.structureType==STRUCTURE_TOWER)[0] as StructureTower;
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    // creep角色逻辑
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
 })
