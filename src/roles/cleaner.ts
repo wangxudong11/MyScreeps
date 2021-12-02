c587d8c4d82fc5867f58f55079394ea6e2d1e84a
王旭东 <wwwxdhouwxd@qq.com>
2021-12-02 14:08:18 +0800

    cleaner、flag文件预创建

---
+++ src/roles/cleaner.ts
@@ -0,0 +1,33 @@
+/**
+ * 清洁工
+ */
+export class roleCleaner {
+
+    /** @param {Creep} creep **/
+    public run(creep: Creep) {
+
+	    if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 build');
+	    }
+
+	    if(creep.memory.building) {
+	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
+	}
+}
