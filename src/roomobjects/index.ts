c1c72fb2a1206db593f23917afeb3704f2379140
王旭东 <wwwxdhouwxd@qq.com>
2021-12-08 17:23:49 +0800

    tower逻辑添加

--- src/roomobjects/index.ts
+++ src/roomobjects/index.ts
@@ -1,10 +1,12 @@
 
 import { assignPrototype } from "@/utils"
 import SpawnExtension from './spawn'
+import TowerExtension from "./tower"
 
 // 拓展和原型的对应关系
 const assignMap = [
     [ StructureSpawn, SpawnExtension ],
+    [ StructureTower, TowerExtension ],
 ]
 
 // 挂载拓展到建筑原型
